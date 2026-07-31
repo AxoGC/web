9920b2f414d8f6aef85b3766d3cd76777eb92def
Sauce233 <2892709432@qq.com>
2026-07-31 14:30:29 +0800

    移动端服务器详情卡片顺序调整为 简介/连接方式/当前在线/趋势/聊天/排行榜/地图
    桌面端仍保持左右两栏分组不变。用 display:contents 让两个分栏容器
    在移动端"消失"，使其子卡片直接成为外层 flex 容器的 flex item，再
    通过 order-*/lg:order-none 分别控制移动端顺序与桌面端（沿用 DOM 顺序）。

--- app/components/feature/server/details/ServerDetailMcJava.vue
+++ app/components/feature/server/details/ServerDetailMcJava.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="flex flex-col lg:flex-row gap-4">
-    <div class="flex flex-col gap-4 lg:w-2/3">
-      <ServerDescriptionCard :server="server" />
-      <ServerOnlineTrendCard :server-id="server.id" />
-      <ServerChatPanel :server-id="server.id" />
-      <ServerBluemapPanel :server="server" />
+  <div class="flex flex-col gap-4 lg:flex-row">
+    <div class="contents lg:flex lg:flex-col lg:gap-4 lg:w-2/3">
+      <ServerDescriptionCard class="order-1 lg:order-none" :server="server" />
+      <ServerOnlineTrendCard class="order-4 lg:order-none" :server-id="server.id" />
+      <ServerChatPanel class="order-5 lg:order-none" :server-id="server.id" />
+      <ServerBluemapPanel class="order-7 lg:order-none" :server="server" />
     </div>
-    <div class="flex flex-col gap-4 lg:w-1/3">
-      <ServerConnectCard :server="server" />
-      <ServerOnlinePlayersCard :server-id="server.id" :type="server.type" :players="server.players" :max="server.max" />
-      <ServerLeaderboardSummary :server-id="server.id" />
+    <div class="contents lg:flex lg:flex-col lg:gap-4 lg:w-1/3">
+      <ServerConnectCard class="order-2 lg:order-none" :server="server" />
+      <ServerOnlinePlayersCard class="order-3 lg:order-none" :server-id="server.id" :type="server.type" :players="server.players" :max="server.max" />
+      <ServerLeaderboardSummary class="order-6 lg:order-none" :server-id="server.id" />
     </div>
   </div>
 </template>
